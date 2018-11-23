7935455472dfe29240050e6b20e69b19d8720f92
Philip Mutua <pmutua@live.com>
2018-11-23 22:29:02 +0300

    Including external dependencies in your

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,9 +11,20 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.abspath(os.path.join(
+os.path.dirname(__file__), ".."
+))
+EXTERNAL_LIBS_PATH = os.path.join(
+BASE_DIR, "externals", "libs"
+)
+EXTERNAL_APPS_PATH = os.path.join(
+BASE_DIR, "externals", "apps"
+)
+sys.path = ["", EXTERNAL_LIBS_PATH, EXTERNAL_APPS_PATH] + \
+sys.path
 
 
 # Quick-start development settings - unsuitable for production
