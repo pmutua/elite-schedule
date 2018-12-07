5df541c06617190a49ee3619dcb7623259dee651
Philip Mutua <pmutua@live.com>
2018-12-07 20:03:47 +0300

    deploy:deploy containerized django rest api to heroku

--- core/urls.py
+++ core/urls.py
@@ -41,10 +41,10 @@ urlpatterns = [
     url(r'^signup/', include('rest_auth.registration.urls'),name='sign-up'),
     url('admin/', admin.site.urls),
     # adding '' results to redirecting to root 
-    url('api/elite_schedule/', include('elite_schedule.urls',elite_schedule)),
+    url('api/elite_schedule/', include('elite_schedule.urls')),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     # TODO leave this with '' to start as root
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # change redoc to  '' 
     url(r'^refresh-token/', refresh_jwt_token),
