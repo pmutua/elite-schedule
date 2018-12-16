71c1180f629f18a6548fa0c32a76edf8ca1a2bfa
Philip Mutua <pmutua@live.com>
2018-12-16 20:23:36 +0300

    config: templates location

--- elite_schedule/views.py
+++ elite_schedule/views.py
@@ -1,10 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
+
 from .models import Match
 
 
 class MatchList(ListView):
     model = Match
     
+class MatchDetail(DetailView):
+    model = Match
+
 class EnglishPremierLeagueMatchList(ListView):
     queryset= (Match.objects.eng_premier_league())
 
