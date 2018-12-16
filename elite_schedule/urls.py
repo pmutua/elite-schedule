4722530feb3fba1d70834e71415717432c55a857
Philip Mutua <pmutua@live.com>
2018-12-16 18:42:15 +0300

    feat: Added extra views for english leagues

--- elite_schedule/urls.py
+++ elite_schedule/urls.py
@@ -7,5 +7,8 @@ app_name = 'elite_schedule'
 
 urlpatterns = [
     path('matches', views.MatchList.as_view(),name='MatchList'),
-    path('english_premier_league', views.EnglishPremierLeagueMatchList.as_view(),name='EnglishPremierLeague')
+    path('english_premier_league', views.EnglishPremierLeagueMatchList.as_view(),name='EnglishPremierLeague'),
+    path('english_conference', views.EnglishConferenceMatchList.as_view(),name='EnglishConference'),
+    path('english_league_1', views.EnglishLeagueOneMatchList.as_view(),name='EnglishLeagueOne'),
+    path('english_league_2', views.EnglishLeagueTwoMatchList.as_view(),name='EnglishLeagueTwo')
 ]
--- elite_schedule/views.py
+++ elite_schedule/views.py
@@ -7,3 +7,12 @@ class MatchList(ListView):
     
 class EnglishPremierLeagueMatchList(ListView):
     queryset= (Match.objects.eng_premier_league())
+
+class EnglishConferenceMatchList(ListView):
+    queryset= (Match.objects.eng_conference())
+
+class EnglishLeagueOneMatchList(ListView):
+    queryset= (Match.objects.eng_league_1())
+
+class EnglishLeagueTwoMatchList(ListView):
+    queryset= (Match.objects.eng_league_2())
